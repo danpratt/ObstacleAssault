560920f619f9226ae1b74c938687cd696aaefe37
Daniel Pratt <daniel@blaumagier.com>
2026-01-29 13:22:36 -0500

    feat: start creating a moving platform class

---
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float MemberFloat = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(-610.0f, 470.0f, 390.0f);
+};
